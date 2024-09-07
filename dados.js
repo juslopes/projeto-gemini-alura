7b10ba2a9dd3ed9bfc689edc04a63acb50aa8f6a
juslopes <136720482+juslopes@users.noreply.github.com>
2024-09-06 22:49:30 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,17 +1,21 @@
+
 //Objeto
 let dados = [{
     titulo: "Fernanda Gimenez | Psicóloga | CRP 06/141226",
     descricao: "Autoconhecimento & autocontrole. O autocuidado vai além de práticas individuais de cuidado físico e mental. <br> Envolve a escolha das pessoas com quem você se cerca. <br> Relações saudáveis e de apoio são essenciais para o bem-estar emocional e psicológico. Parte do autocuidado é também cultivar um ambiente social que favoreça seu bem-estar. <br> Agora me diz aqui, você consegue perceber alguém que não te faz bem ?!",
-    link: "https://www.instagram.com/psi.fernandagimenez/"
+    link: "https://www.instagram.com/psi.fernandagimenez/",
+    tags: "autoconhecimento, pânico, terapia"
 }, 
 {
     titulo: "Stella Girasole | Terapia Cognitiva Comportamental - CRP 167336",
     descricao: "Antes de tudo, a terapia é um espaço do paciente. <br> É o lugar aonde se revela sua história de vida, possíveis traumas, dificuldades e emoções. Com ajuda profissional, o objetivo será que passe a lidar melhor com seus pensamentos e comportamentos, se sentirá mais segura ou seguro em tomadas de decisões, ressignificando seus problemas e se relacionando de forma mais saudável consigo, e com os outros. Atuo com a TCC “terapia cognitiva comportamental” e trata-se de uma abordagem baseada em evidências científicas e, portanto, proporciona recursos e técnicas eficazes para resolução de problemas. <br> Não se trata de 'adestramento', fique tranquila(o). <br> Para lidarmos com quaisquer problemas, antes de qualquer coisa, é necessário que se construa um bom vínculo terapêutico entre mim e meus pacientes - acho esse ponto muito importante e valioso, e me dedico muito a construí-lo! Me chamo Stella Girasole Dias, tenho 29 anos e moro em São Paulo. <br> Atuo como psicóloga clínica, mas também já atuei dois anos como psicóloga hospitalar em paralelo - duas áreas pelas quais sou apaixonada e nas quais me realizei por conseguir conciliá-las por tanto tempo. Sou formada em Psicologia pela Universidade Paulista (UNIP), especialista em Psicopatologia e Saúde Mental pelo CEPPS - Centro de Estudos e Pesquisas em Psicologia e Saúde, aluna dos cursos de formação em Psicopatologia e Terapia Cognitiva Comportamental pelo Grupo PBE Educação, além de participante do Projeto de Formação Continuada na Prática Clínica também do Grupo PBE, que me proporciona constantes atualizacões através de supervisões semanais de casos clínicos. <br> Sou aluna, também, do curso de pós graduação em Psicologia Perinatal e da Parentalidade pelo Instituto Mater Online, e possuo cursos livres em Transtorno Ansioso Depressivo e Avaliação Hospitalar pelo Instituto Israelita de Ensino e Pesquisa Albert Einstein, além de capacitação completa em Prevenção ao Suicídio pelo Espaço Amarelo.",
-    link: "https://www.instagram.com/stellagirasolepsi/"
+    link: "https://www.instagram.com/stellagirasolepsi/",
+    tags: "toc, transtornos, terapia"
 },   
 {
     titulo: "Dulcinda Alves | Psicanálista Naturopatia",
     descricao: "Terapia com Alma mudam o sistema dentro pra fora. Terapeuta em Psicanalise Clinica, Complementar e Pesquisadora  em Sicronicidade , com especialização em Bioeletrografia pela IUMAB <br> Internacional Union of Medica And Applied Bioeletrography (orgão maximo- em âmbito mundial em Bioeletrografia Reconhecida) , com Formação e especialização nas mais diversas áres congêneres,  atuando com Medicina Alternativa, Terapias em geral, Terapeuta Corporal e emocional, Mestra Reike, Cromoterapia, Hipnoterapia, Terapia de Vidas Passadas, acupunturista, Massoterapia, Terapeuta Transpessoal. <br> Dez anos de experiência na área da saúde em terapias complementares, especialista em Bioeletrografia. Desenvolvi uma metodologia de ensino teórico e prático nos cursos, sob o paradigma holístico e abordagem científica. <br>O objetivo e formar pessoas capazes de diagnosticar e aperfeiçoar os processos terapêuticos holísticos. No curso, desenvolvesse habilidades específicas que dão apoio aos participantes a serem capazes de atingir seus objetivos em áreas-chave de sua vida pessoal, e ajudar as pessoas a praticar o autoequilíbrio.",
-    link: "https://www.instagram.com/dulcinda_alves/"
+    link: "https://www.instagram.com/dulcinda_alves/",
+    tags: "terapia, ajuda"
 }
 ]
